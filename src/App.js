c6d51289a4a5af8891a634fbb7e09ef8cb471b40
akane <a@com>
2025-07-16 19:32:06 +0900

    creatpost,login,logoutのimport

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
 
 
 function App() {
@@ -8,9 +11,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<CreatePost />}></Route>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/" element={<Logout />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
   );
