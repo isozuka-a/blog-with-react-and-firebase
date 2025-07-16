7577326f6e1ed7a79a5b1d996c6885d17f1e53a6
akane <a@com>
2025-07-16 19:25:36 +0900

    createpost,login,logoutのコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<CreatePost />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/" element={<Logout />}></Route>
       </Routes>
     </Router>
   );
